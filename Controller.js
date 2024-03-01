1758ab6b8f97ff13dc2aaf8df0e26633ccc78e03
donikorch <DoniKorch48@yandex.ru>
2024-03-01 15:30:26 +0300

    release controller

--- Controller.js
+++ Controller.js
@@ -1,5 +1,9 @@
-class Controller{
-    constructor(){
-        
-    }
+const View = require('./View');
+
+class Controller {
+  static async runQuiz() {
+    await new View().show();
+  }
 }
+
+Controller.runQuiz();
